1bce36477796d64cddfcc8df84b25e44ff224dab
Yeoh Hui Qing <yeohhuiqing00@gmail.com>
2023-11-26 16:36:30 +0800

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Yeoh Hui Qing on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
